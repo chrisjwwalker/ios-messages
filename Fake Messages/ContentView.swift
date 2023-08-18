ae8d92f4c7915d340134bebb7a6d923d7e70d0df
chrisjwalker <8091191+chrisjwwalker@users.noreply.github.com>
2023-08-18 21:39:08 +0100

    Initial project

---
+++ Fake Messages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fake Messages
+//
+//  Created by Chris J W Walker on 18/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fake Messages/Fake_MessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fake_MessagesApp.swift
+//  Fake Messages
+//
+//  Created by Chris J W Walker on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fake_MessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
